48160a03e2b04a1361218e264524fbbe1e9b7827
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-22 10:00:30 +0200

    new jumbo

--- src/components/HeroJumbo.vue
+++ src/components/HeroJumbo.vue
@@ -1,7 +1,13 @@
 <template>
 
-    <div class="img-container">
-        <img src="../assets/jumbo.png" alt="jumbo">
+    <div class="jumbo-container">
+        <h2 class="jumbo-title">Questo è un jumbotron!</h2>
+        <div class="jumbo-caption">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur eum nostrum nemo, impedit incidunt
+            temporibus totam dignissimos! Obcaecati sequi vel veniam non aut mollitia reiciendis impedit debitis
+            incidunt tenetur commodi voluptatum unde, earum excepturi, voluptas nisi omnis ipsam error veritatis
+            molestias esse? Cupiditate numquam deserunt repudiandae voluptates voluptatibus repellat earum.
+        </div>
     </div>
 
 </template>
@@ -13,7 +19,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
-img {
-    width: 100%
+.jumbo-container {
+    min-height: 80vh;
+    background-image: url(../assets/jumbo.png);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.jumbo-caption {
+    width: 50%;
 }
 </style>
